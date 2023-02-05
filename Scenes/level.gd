2070ab655bbc13d5ff3750aedbe6a35598199a38
nik <124413898+its-moda@users.noreply.github.com>
2023-02-05 08:42:40 -0800

    Revert "Flamingo and updated Cutscene"
    This reverts commit 63086806070f89227e87f5e70f4bf882282b240e.

--- Scenes/level.gd
+++ Scenes/level.gd
@@ -1,16 +1,12 @@
 extends Node2D
 
-
-@onready var womanwalk2 = preload("res://Scenes/woman_walk_2.tscn")
 var bgmusic = $AudioStreamPlayer
-var introtimer = $Timer_wwp
-
+var introtimer = $Timer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Camera2D.current = true
-	$Timer_wwp.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,7 +21,3 @@ func _input(event):
 		$Camera2D.position.x = event.position.x + ($Camera2D.get_viewport_rect().size.x / 4)
 		print($Camera2D.position)
 
-
-#Woman Walk to Pot and Water Call
-func _on_timer_wwp_timeout():
-	womanwalk2.instantiate()
--- intro_cut.gd
+++ intro_cut.gd
@@ -1,4 +1,5 @@
 extends Node2D
+var levelload = preload("res://Scenes/level.tscn")
 var walkloadman = preload("res://Scenes/Man_Walk_1.tscn")
 var walkloadwoman = preload("res://Scenes/woman_walk_1.tscn")
 var dialogtriggered = 0
@@ -13,14 +14,9 @@ func walkout():
 	add_child(womanwalk)
 	var manwalk = walkloadman.instantiate()
 	add_child(manwalk)
-	$Timer2.start()
 
 func _process(delta):
 	pass
 
 func calldialog():
 	Dialogic.start("couple_talk1")
-
-
-func _on_timer_2_timeout():
-	get_tree().change_scene_to_file("res://Scenes/level.tscn")
