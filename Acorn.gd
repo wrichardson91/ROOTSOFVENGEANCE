5c4b3c61ff592f21e6bdee585aea338e45e1951d
wrichardson91 <104599624+wrichardson91@users.noreply.github.com>
2023-02-05 11:21:35 -0600

    Fixed cutscene

--- Acorn.gd
+++ Acorn.gd
@@ -3,13 +3,12 @@ extends RigidBody2D
 var gravityOff: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
 func launch(force: Vector2):
 	print("FORCE-------------", force )
 	$".".apply_impulse(force)
